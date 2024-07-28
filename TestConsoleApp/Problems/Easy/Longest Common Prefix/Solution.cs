eaf6be91e4a04b42d5724832f009386abd156b7e
hoanganhminh <hoanganhminh120601@gmail.com>
2024-07-28 18:11:12 +0700

    Added new solution

---
+++ TestConsoleApp/Problems/Easy/Longest Common Prefix/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace TestConsoleApp.Problems.Easy.Longest_Common_Prefix;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        var commonPrefix = "";
+        var firstStringCharList = strs[0].ToCharArray();
+        var checkDifferent = false;
+
+        for (int i = 0; i < firstStringCharList.Length; i++)
+        {
+            for (int j = 0; j < strs.Length; j++)
+            {
+                if(j + 1 >= strs.Length)
+                    break;
+
+                if (i >= strs[j + 1].ToCharArray().Length || firstStringCharList[i] != strs[j + 1].ToCharArray()[i])
+                {
+                    checkDifferent = true;
+                    break;
+                }
+            }
+
+            if (checkDifferent)
+                break;
+
+            commonPrefix += firstStringCharList[i].ToString();
+        }
+
+        return commonPrefix;
+    }
+}
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,7 +1,12 @@
-﻿using TestConsoleApp.Problems.Easy.Roman_to_Integer;
+﻿//using TestConsoleApp.Problems.Easy.Roman_to_Integer;
+
+//Solution RomanToIntegerSolution = new Solution();
+using TestConsoleApp.Problems.Easy.Longest_Common_Prefix;
 
 Solution RomanToIntegerSolution = new Solution();
 
-var result = RomanToIntegerSolution.RomanToInt("III");
+
+
+var result = RomanToIntegerSolution.LongestCommonPrefix(["ab", "a"]);
 
 Console.WriteLine(result);
