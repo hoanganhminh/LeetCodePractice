89807a70e9a9d9b74022a64c1785a623a6e0419c
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-04 01:19:03 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Remove Element/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace TestConsoleApp.Problems.Easy.Remove_Element;
+
+public class Solution
+{
+    public int RemoveElement(int[] nums, int val)
+    {
+        var arr = nums.Where(x => x != val).ToArray();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (i < arr.Length)
+            {
+                nums[i] = arr[i];
+            }
+        }
+
+        return arr.Length;
+    }
+}
