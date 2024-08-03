047c4d01eba291cee461395aa1d78e09039c7dee
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-04 00:34:09 +0700

    Add solution

---
+++ TestConsoleApp/Problems/Easy/Merge Sorted Array/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace TestConsoleApp.Problems.Easy.Merge_Sorted_Array;
+
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        int[] tempNums1 = nums1.Take(m).Concat(nums2.Take(n)).Order().ToArray();
+
+        for (int i = 0; i < m + n ; i++)
+        {
+            nums1[i] = tempNums1[i];
+        }
+    }
+}
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,7 +1,8 @@
-﻿using TestConsoleApp.Problems.Easy.Valid_Parentheses;
+﻿using TestConsoleApp.Problems.Easy.Merge_Sorted_Array;
 
-Solution Valid_Parentheses = new Solution();
+Solution Merge_Sorted_Array = new Solution();
+int[] num1 = [1, 2, 3, 0, 0, 0];
+int[] num2 = [2, 5, 6];
+Merge_Sorted_Array.Merge(num1, 3,num2 , 3);
 
-var result = Valid_Parentheses.IsValid("){");
-
-Console.WriteLine(result);
+//Console.WriteLine(result);
