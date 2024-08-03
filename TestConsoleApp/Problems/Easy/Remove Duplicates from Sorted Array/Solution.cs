b10688003fe939bd8fdaf2f4ff495716ebc181ce
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-04 02:10:34 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Remove Duplicates from Sorted Array/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace TestConsoleApp.Problems.Easy.Remove_Duplicates_from_Sorted_Array;
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+        var arr = nums.Distinct().ToArray();
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            nums[i] = arr[i];
+        }
+
+        return arr.Length;
+    }
+}
