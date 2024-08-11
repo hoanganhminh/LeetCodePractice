ef6356df37836254209308165327f766a44f9523
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-12 00:22:19 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Plus One/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace TestConsoleApp.Problems.Easy.Plus_One;
+
+public class Solution
+{
+    public int[] PlusOne(int[] digits)
+    {
+        BigInteger num = 0;
+
+        foreach (int i in digits)
+        {
+            num = (num + i) * 10;
+        }
+
+        num = num / 10 + 1;
+        char[] chars = num.ToString().ToCharArray();
+
+        for (int i = 0; i < chars.Length; i++)
+        {
+            if (i < digits.Length)
+            {
+                digits[i] = int.Parse(chars[i].ToString());
+            }
+            else
+            {
+                return digits.Append(int.Parse(chars[i].ToString())).ToArray();
+            }
+        }
+
+        return digits;
+    }
+}
