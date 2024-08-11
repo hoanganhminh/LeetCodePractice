78f6954398f6cf76e8a13486a50d23fd8684c77f
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-11 21:52:11 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Length of Last Word/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace TestConsoleApp.Problems.Easy.Length_of_Last_Word;
+
+public class Solution
+{
+    public int LengthOfLastWord(string s)
+    {
+        if(s.Trim().Length == 0) return 0;
+
+        return s.Trim().Split(' ').Last().Length;
+    }
+}
