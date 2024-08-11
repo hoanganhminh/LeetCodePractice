ed72954f7c4dfa533b61e634fadfb199775579a6
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-11 18:53:42 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Search Insert Position/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace TestConsoleApp.Problems.Easy.Search_Insert_Position;
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        if (!nums.Contains(target))
+        {
+            nums = nums.Append(target).Order().ToArray();
+        }
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+                return i;
+        }
+
+        return 0;
+    }
+}
