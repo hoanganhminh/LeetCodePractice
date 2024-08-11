baf047d6a3d6dcbd0e5617f486bf4d3a0e10e9df
hoanganhminh <hoanganhminh120601@gmail.com>
2024-08-11 18:53:33 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Find the Index of the First Occurrence in a String/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace TestConsoleApp.Problems.Easy.Find_the_Index_of_the_First_Occurrence_in_a_String;
+
+public class Solution
+{
+    public int StrStr(string haystack, string needle)
+    {
+        return haystack.IndexOf(needle);
+    }
+}
