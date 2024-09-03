15d426e8107b043312a2c067f922947b758b5cc1
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-03 13:27:58 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Convert 1D Array Into 2D Array/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace TestConsoleApp.Problems.Easy.Convert_1D_Array_Into_2D_Array;
+
+public class Solution
+{
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        bool validate = (m < 1) || (original.Length / n < 1) || (m != (original.Length / n) + (original.Length % n));
+
+        if (validate)
+        {
+            return [];
+        }
+
+        int[][] a = new int[m][];
+
+        for (int i = 0; i < m; i++)
+        {
+            a[i] = original.Skip(i * n).Take(n).ToArray();
+        }
+
+        return a;
+    }
+}
