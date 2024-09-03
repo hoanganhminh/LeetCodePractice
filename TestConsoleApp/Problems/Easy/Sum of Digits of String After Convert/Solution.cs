403dd9fea2c722d86f521114fb93531b6c9b9da2
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-03 22:49:51 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Sum of Digits of String After Convert/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace TestConsoleApp.Problems.Easy.Sum_of_Digits_of_String_After_Convert;
+
+public class Solution
+{
+    public int GetLucky(string s, int k)
+    {
+        s = s.ToLower();
+        char[] chars = s.ToCharArray();
+        s = "";
+        int sum = 0;
+
+        foreach (char c in chars)
+        {
+            s += ((int)c - 96);
+        }
+
+        for (int i = 0; i < k; i++)
+        {
+            sum = 0;
+
+            foreach (var item in s.ToCharArray())
+            {
+                sum += int.Parse(item.ToString());
+            };
+
+            s = sum.ToString();
+        }
+
+        return sum;
+    }
+}
