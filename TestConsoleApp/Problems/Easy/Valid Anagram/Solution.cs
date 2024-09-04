f9ce5e9b810c7769c79d94b52e69e5cee6b2ac2b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-04 20:46:43 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Valid Anagram/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace TestConsoleApp.Problems.Easy.Valid_Anagram;
+
+public class Solution
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if (s.Length != t.Length) return false;
+
+        var c1 = s.ToCharArray().Order().ToArray();
+        var c2 = t.ToCharArray().Order().ToArray();
+
+        for (int i = 0; i < c1.Length; i++)
+        {
+            if (c1[i] != c2[i])
+                return false;
+        }
+
+        return true;
+    }
+}
