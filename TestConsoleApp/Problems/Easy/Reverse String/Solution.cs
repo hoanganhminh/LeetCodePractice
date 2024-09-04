1bfb2d44170f44d2eb38d56509bea7895945f0c4
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-04 20:46:32 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Reverse String/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace TestConsoleApp.Problems.Easy.Reverse_String;
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        var rv = s.Reverse().ToArray();
+        for (int i = 0; i < s.Length; i++)
+        {
+            s[i] = rv[i];
+        }
+    }
+}
