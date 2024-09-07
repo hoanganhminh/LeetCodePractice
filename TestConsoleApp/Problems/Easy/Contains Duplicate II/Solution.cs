90f0e9dd0642e5d39d844a557ebb64be9ee7a6ce
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-07 23:20:14 +0700

    Added solutions

---
+++ TestConsoleApp/Problems/Easy/Contains Duplicate II/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace TestConsoleApp.Problems.Easy.Contains_Duplicate_II;
+
+public class Solution
+{
+    public bool ContainsNearbyDuplicate(int[] nums, int k)
+    {
+        if (nums.Length == nums.Distinct().Count())
+            return false;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j] && Math.Abs(i - j) <= k)
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
