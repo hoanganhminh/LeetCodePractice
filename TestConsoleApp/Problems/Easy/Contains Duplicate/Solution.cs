cf0238cea925f9d7db03835703c94808a908dfbf
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-07 16:19:53 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Contains Duplicate/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace TestConsoleApp.Problems.Easy.Contains_Duplicate;
+
+public class Solution
+{
+    public bool ContainsDuplicate(int[] nums)
+    {
+        bool rs = nums.Length != nums.Distinct().Count();
+        return rs;
+    }
+}
