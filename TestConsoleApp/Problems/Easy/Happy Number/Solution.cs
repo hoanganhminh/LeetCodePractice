3e5ddc37aa4dfe9c2c7ab2b1e308d00f4dc902ae
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-08 23:51:28 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Happy Number/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace TestConsoleApp.Problems.Easy.Happy_Number;
+
+public class Solution
+{
+    public bool IsHappy(int n)
+    {
+        int val = 0;
+
+        while (n - 10 >= 0)
+        {
+            foreach (char a in n.ToString().ToCharArray())
+            {
+                val += (int)Math.Pow(int.Parse(a.ToString()), 2);
+            }
+
+            n = val;
+            val = 0;
+        }
+
+        return n == 1 || n == 7 ? true : false;
+    }
+}
