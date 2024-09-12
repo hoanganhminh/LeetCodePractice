673787b5ed81a9cd589811039375baef6a61b42b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-12 23:54:59 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Count the Number of Consistent Strings/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace TestConsoleApp.Problems.Easy.Count_the_Number_of_Consistent_Strings;
+
+public class Solution
+{
+    public int CountConsistentStrings(string allowed, string[] words)
+    {
+        int invalid = 0;
+
+        foreach (var item in words)
+        {
+            foreach (char c in item)
+            {
+                if (!allowed.Contains(c))
+                {
+                    invalid++;
+                    break;
+                }
+            }
+        }
+
+        return words.Length - invalid;
+    }
+}
