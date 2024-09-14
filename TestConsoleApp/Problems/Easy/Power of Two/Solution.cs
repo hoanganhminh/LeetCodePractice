5068453c9dad2da9671991562d25def5bd7729ff
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-14 14:23:20 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Power of Two/Solution.cs
@@ -0,0 +1,13 @@
+﻿namespace TestConsoleApp.Problems.Easy.Power_of_Two;
+
+public class Solution
+{
+    public bool IsPowerOfTwo(int n)
+    {
+        if (n < 1) return false;
+
+        var nBinary = Convert.ToString(n, 2);
+
+        return (nBinary[0] == '1' && nBinary.Split('1').Length == 2);
+    }
+}
