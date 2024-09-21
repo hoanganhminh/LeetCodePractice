b9271358d05b65f80ad8840e384d153066d4d777
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-21 19:32:57 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Valid Palindrome/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace TestConsoleApp.Problems.Easy.Valid_Palindrome;
+
+public class Solution
+{
+    public bool IsPalindrome(string s)
+    {
+        s = s.Trim();
+
+        if (s == "")
+        {
+            return true;
+        }
+
+        var lettersOnly = s.ToLower().Where(x => char.IsLetterOrDigit(x)).ToList();
+
+
+        if (lettersOnly.Count == 1)
+            return true;
+
+        int position = lettersOnly.Count % 2 == 0 ? 0 : 1;
+        var f = lettersOnly.Skip(0).Take((lettersOnly.Count) / 2).ToList();
+        var l = lettersOnly.Skip((lettersOnly.Count) / 2 + position).Reverse().ToList();
+
+        return Enumerable.SequenceEqual(f, l);
+    }
+}
--- TestConsoleApp/Program.cs
+++ TestConsoleApp/Program.cs
@@ -1,9 +1,4 @@
-﻿using TestConsoleApp.Problems.Easy.Add_Binary;
+﻿//Solution slt = new Solution();
 
-Solution Add_Binary = new Solution();
-int[] nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
-string num1 = "11";
-string num2 = "1";
-var result = Add_Binary.AddBinary(num1, num2);
+//Console.WriteLine(slt.IsPalindrome("aa"));
 
-Console.WriteLine(result);
