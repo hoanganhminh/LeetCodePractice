ed61191f2e63bff9e5a2e8dcc6f312f02ed590f1
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-22 15:25:10 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Multiply Strings/Solution.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace TestConsoleApp.Problems.Medium.Multiply_Strings;
+
+public class Solution
+{
+    public string Multiply(string num1, string num2)
+    {
+        return (BigInteger.Parse(num1) * BigInteger.Parse(num2)) + "";
+    }
+}
