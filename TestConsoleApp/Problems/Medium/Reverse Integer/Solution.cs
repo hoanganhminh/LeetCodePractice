05765bb6aedaeef07ad0ff60000063980ee622ff
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-22 16:43:09 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Reverse Integer/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace TestConsoleApp.Problems.Medium.Reverse_Integer;
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        int minor = 1;
+        int rs = 0;
+
+        if (x < 0)
+        {
+            minor = -1;
+            x *= minor;
+        }
+
+        char[] rv = x.ToString().ToCharArray();
+        Array.Reverse(rv);
+
+        return Int32.TryParse(rv, out rs) ? rs * minor : rs;
+    }
+}
