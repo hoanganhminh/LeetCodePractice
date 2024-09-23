ccfb52756c0b5d5a18b7a892cf37f9b440db9bee
hoanganhminh <hoanganhminh120601@gmail.com>
2024-09-23 23:56:35 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Day of the Year/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace TestConsoleApp.Problems.Easy.Day_of_the_Year;
+
+public class Solution
+{
+    public int DayOfYear(string date)
+    {
+        return DateTime.Parse(date).DayOfYear;
+    }
+}
