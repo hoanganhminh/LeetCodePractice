1932eb2758cd4b280521f736942d88e6652315de
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-02 23:55:20 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Single Number/Solution.cs
@@ -0,0 +1,51 @@
+﻿namespace TestConsoleApp.Problems.Easy.Single_Number;
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        bool flag = true;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            flag = true;
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                if (nums[j] == nums[i])
+                {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag)
+            {
+                return nums[i];
+            }
+        }
+
+        return 0;
+    }
+
+    //public int SingleNumber(int[] nums)
+    //{
+    //    if (nums.Length == 1)
+    //        return nums[0];
+
+    //    foreach (int num in nums)
+    //    {
+    //        if (nums.Where(x => x == num).Count() == 1)
+    //        {
+    //            return num;
+    //        }
+    //    }
+
+    //    return 0;
+    //}
+}
