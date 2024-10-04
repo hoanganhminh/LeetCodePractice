5886990e5496e7de3c5374d2cbaf2e6017fd43a8
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-05 00:13:57 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Shuffle String/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace TestConsoleApp.Problems.Easy.Shuffle_String;
+
+public class Solution
+{
+    public string RestoreString(string s, int[] indices)
+    {
+        char[] rs = new char[s.Length];
+        for (int i = 0; i < s.Length; i++)
+        {
+            rs[indices[i]] = s[i];
+        }
+
+        return new string(rs);
+    }
+}
