e099155a169c99d0d8b4b97c2575d5f53612bbac
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-06 11:06:15 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Reformat Phone Number/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace TestConsoleApp.Problems.Easy.Reformat_Phone_Number;
+
+public class Solution
+{
+    public string ReformatNumber(string number)
+    {
+        var formatedNumber = "";
+        var count = 0;
+        for (int i = 0; i < number.Length; i++)
+        {
+            if (number[i] == ' ' || number[i] == '-')
+            {
+                continue;
+            }
+
+            if (count == 3)
+            {
+                formatedNumber += "-";
+                count = 0;
+            }
+
+            formatedNumber += number[i];
+            count++;
+        }
+
+        if (formatedNumber[formatedNumber.Length - 2] == '-')
+        {
+            var reFormated = "-" + formatedNumber[formatedNumber.Length - 3] + formatedNumber[formatedNumber.Length - 1];
+            formatedNumber = formatedNumber.Remove(formatedNumber.Length - 3) + reFormated;
+        }
+
+        return formatedNumber;
+    }
+}
