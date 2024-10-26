17ea78acb25fec65ac8187cddc8aa6fe7cb96b1d
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-26 13:32:28 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Number Complement/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace TestConsoleApp.Problems.Easy.Number_Complement;
+
+public class Solution
+{
+    public int FindComplement(int num)
+    {
+        var binary = Convert.ToString(num, 2);
+        var complement = "";
+        foreach (char c in binary)
+        {
+            complement += c == '1' ? 0 : 1;
+        }
+        return Convert.ToInt32(complement, 2);
+    }
+}
