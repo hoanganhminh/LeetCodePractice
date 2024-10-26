e1d58c1edfea50072fc9505508331fb2d0e391d2
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-26 13:35:11 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Uncommon Words from Two Sentences/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace TestConsoleApp.Problems.Easy.Uncommon_Words_from_Two_Sentences;
+
+public class Solution
+{
+    public string[] UncommonFromSentences(string s1, string s2)
+    {
+        var arr = s2.Split(' ').ToList();
+        arr.AddRange(s1.Split(' '));
+
+        var rs = new List<string>();
+
+        arr.ForEach(x => 
+        {
+            if (!arr.Contains(x))
+            {
+                rs.Add(x);
+            } 
+        });
+
+        return rs.ToArray();
+    }
+}
