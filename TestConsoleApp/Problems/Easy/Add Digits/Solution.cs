00aaadbe307f7694d20cabb59e3b07ecb80cc608
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-27 15:44:52 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Add Digits/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace TestConsoleApp.Problems.Easy.Add_Digits;
+
+public class Solution
+{
+    public int AddDigits(int num)
+    {
+        while (num > 9)
+        {
+            var num2 = 0;
+
+            foreach (var item in num.ToString().ToCharArray())
+            {
+                num2 += item - '0';
+            };
+
+            num = num2;
+        }
+
+        return num;
+    }
+}
