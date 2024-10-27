8bc421ff25237afbc429653128c0436ac12e11c0
hoanganhminh <hoanganhminh120601@gmail.com>
2024-10-27 15:44:38 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Complement of Base 10 Integer/Solution.cs
@@ -0,0 +1,15 @@
+﻿namespace TestConsoleApp.Problems.Easy.Complement_of_Base_10_Integer;
+
+public class Solution
+{
+    public int BitwiseComplement(int n)
+    {
+        var binary = Convert.ToString(n, 2);
+        var complement = "";
+        foreach (char c in binary)
+        {
+            complement += c == '1' ? 0 : 1;
+        }
+        return Convert.ToInt32(complement, 2);
+    }
+}
