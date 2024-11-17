ee08e53d2c0d7d70b35bd7cf448bb027a23abb3b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-17 23:30:27 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Find the Difference/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace TestConsoleApp.Problems.Easy.Find_the_Difference;
+
+public class Solution
+{
+    public char FindTheDifference(string s, string t)
+    {
+        if (s == "")
+        {
+            return t[0];
+        }
+
+        var tList = t.ToCharArray().ToList();
+
+        foreach (char x in s)
+        {
+            tList.Remove(x);
+        }
+
+        return tList[0];
+    }
+}
