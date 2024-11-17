424d0d4192059fef0efd1514e01e94e6a5e96802
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-17 23:31:19 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Word Pattern/Solution.cs
@@ -0,0 +1,37 @@
+﻿namespace TestConsoleApp.Problems.Easy.Word_Pattern;
+
+public class Solution
+{
+    public bool WordPattern(string pattern, string s)
+    {
+        var stringArr = s.Split(' ');
+
+        if (stringArr.Length != pattern.Length)
+        {
+            return false;
+        }
+
+        Dictionary<char, string> dic = [];
+
+        for (int i = 0; i < pattern.Length; i++)
+        {
+            if (dic.ContainsKey(pattern[i])
+                && dic.GetValueOrDefault(pattern[i]) != stringArr[i])
+            {
+                return false;
+            }
+
+            if (!dic.ContainsKey(pattern[i]))
+            {
+                if (dic.FirstOrDefault(x => x.Value == stringArr[i]).Value != null)
+                {
+                    return false;
+                }
+
+                dic.Add(pattern[i], stringArr[i]);
+            }
+        }
+
+        return true;
+    }
+}
