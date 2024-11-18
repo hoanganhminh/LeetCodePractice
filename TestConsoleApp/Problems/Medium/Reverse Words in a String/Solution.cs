19acd88bc0f21d5a1291da7ea6a21f9717fc0dd8
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-19 00:18:34 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Reverse Words in a String/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace TestConsoleApp.Problems.Medium.Reverse_Words_in_a_String;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        var sArr = s.Split(" ").Where(x => x != "").Reverse();
+
+        return string.Join(" ", sArr);
+    }
+}
