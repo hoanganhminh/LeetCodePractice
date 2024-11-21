547389f2187e442c9e35c3e5aac2d57138fc9a2f
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-21 21:45:23 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/To Lower Case/Solution.cs
@@ -0,0 +1,23 @@
+﻿namespace TestConsoleApp.Problems.Easy.To_Lower_Case;
+
+public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        var result = string.Empty;
+
+        foreach (char c in s)
+        {
+            if (c >= 'A' && c <= 'Z')
+            {
+                result += (char)(c + 32);
+            }
+            else
+            {
+                result += c;
+            }
+        }
+
+        return result;
+    }
+}
