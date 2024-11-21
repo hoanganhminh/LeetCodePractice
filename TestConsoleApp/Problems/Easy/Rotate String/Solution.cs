42bace043974cc3cf8b4470a11c5e491b180c77d
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-22 00:20:39 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Rotate String/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace TestConsoleApp.Problems.Easy.Rotate_String;
+
+public class Solution
+{
+    public bool RotateString(string s, string goal)
+    {
+        foreach (var c in s)
+        {
+            s = s.Substring(1, s.Length - 1);
+            s += c;
+
+            if (s == goal)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
