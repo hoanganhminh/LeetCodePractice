30728854c8c93e9725432adf11b7b1fa46c7cd5b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-22 00:44:19 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Move Zeroes/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace TestConsoleApp.Problems.Easy.Move_Zeroes;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        var copyNums = nums.Where(x => x != 0).ToArray();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            nums[i] = i >= copyNums.Length ? 0 : copyNums[i];
+        }
+    }
+}
