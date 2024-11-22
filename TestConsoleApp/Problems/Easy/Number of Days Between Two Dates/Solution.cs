2f59e2cbe545584dacf33ce78a7d5c10aa313d3b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-23 01:34:53 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Number of Days Between Two Dates/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace TestConsoleApp.Problems.Easy.Number_of_Days_Between_Two_Dates;
+
+public class Solution
+{
+    public int DaysBetweenDates(string date1, string date2)
+    {
+        var from = DateTime.Parse(date1);
+        var to = DateTime.Parse(date2);
+
+        return (int)Math.Abs((from - to).TotalDays);
+    }
+}
