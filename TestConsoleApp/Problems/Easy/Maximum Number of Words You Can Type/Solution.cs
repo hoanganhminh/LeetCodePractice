0823b0de216a9165775946f5e4824a30fe4846cf
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-22 22:17:23 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Maximum Number of Words You Can Type/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace TestConsoleApp.Problems.Easy.Maximum_Number_of_Words_You_Can_Type;
+
+public class Solution
+{
+    public int CanBeTypedWords(string text, string brokenLetters)
+    {
+        var sArr = text.Split(' ');
+
+        return sArr.Where(x => !CheckMacth(x, brokenLetters.ToCharArray())).Count();
+    }
+
+    private bool CheckMacth(string text, char[] chars)
+    {
+        foreach (var c in chars)
+        {
+            if (text.Contains(c))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
