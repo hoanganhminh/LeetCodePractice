d91285f557ae835e6659a367bbd9bbff2ccc23bb
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-25 22:33:55 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Majority Element/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace TestConsoleApp.Problems.Easy.Majority_Element;
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        foreach (var item in nums.ToHashSet())
+        {
+            if (nums.Where(x => x == item).Count() > nums.Length / 2)
+            {
+                return item;
+            }
+        }
+
+        return 0;
+    }
+}
