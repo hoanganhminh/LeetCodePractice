9b69bfff00191f7cf4b680b65b910099dd38425c
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-25 20:59:45 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Max Consecutive Ones/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace TestConsoleApp.Problems.Easy.Max_Consecutive_Ones;
+
+public class Solution
+{
+    public int FindMaxConsecutiveOnes(int[] nums)
+    {
+        var max = 0;
+        var currentMax = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            currentMax = nums[i] == 1 ? currentMax + 1 : 0;
+
+            if (currentMax > max)
+            {
+                max = currentMax;
+            }
+        }
+
+        return max;
+    }
+}
