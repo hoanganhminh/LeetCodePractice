64f00ffb503a3a54bb6ae3f527f372c09267f428
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-26 21:38:26 +0700

    Updated solution

--- TestConsoleApp/Problems/Easy/Two Sum/Solution.cs
+++ TestConsoleApp/Problems/Easy/Two Sum/Solution.cs
@@ -4,15 +4,16 @@ public class Solution
 {
     public int[] TwoSum(int[] nums, int target)
     {
+        Dictionary<int, int> dic = [];
+
         for (int i = 0; i < nums.Length; i++)
         {
-            for (int j = i + 1; j < nums.Length; j++)
+            if (dic.TryGetValue(nums[i], out int val))
             {
-                if ((nums[i] + nums[j]) == target)
-                {
-                    return [i, j];
-                }
+                return [i, val];
             }
+
+            dic.TryAdd(target - nums[i], i);
         }
 
         return [];
