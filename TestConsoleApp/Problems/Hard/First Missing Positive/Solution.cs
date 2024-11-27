0ff6bebdd370c0dd9420df470adcc8508d651534
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-28 00:09:18 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Hard/First Missing Positive/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Hard.First_Missing_Positive;
+
+public class Solution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        Dictionary<int, int> dic = [];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] > 0)
+            {
+                dic.TryAdd(nums[i], i);
+            }
+        }
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!dic.ContainsKey(i))
+            {
+                return i;
+            }
+        }
+
+        return nums.Length + 1;
+    }
+}
