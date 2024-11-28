310466502b1a7a964f88048c28ee8c186ec6855f
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-28 23:39:02 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Find the Duplicate Number/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Medium.Find_the_Duplicate_Number;
+
+public class Solution
+{
+    public int FindDuplicate(int[] nums)
+    {
+        Dictionary<int, bool> dic = [];
+
+        foreach (var i in nums)
+        {
+            if (!dic.TryAdd(i, true))
+            {
+                return i;
+            }
+        }
+
+        return 0;
+    }
+}
