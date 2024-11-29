03bec440e1c10648c89e7125f6692ea184267220
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-29 22:29:06 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Duplicate Zeros/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy.Duplicate_Zeros;
+
+public class Solution
+{
+    public void DuplicateZeros(int[] arr)
+    {
+        var length = arr.Length;
+        var arrList = arr.ToList();
+        var count = 0;
+
+        for (int i = 0; i < length; i++)
+        {
+            if (arr[i] == 0)
+            {
+                arrList.Insert(i + count, 0);
+                count++;
+            }
+        }
+
+        for (int i = 0; i < length; i++)
+        {
+            arr[i] = arrList[i];
+        }
+    }
+}
