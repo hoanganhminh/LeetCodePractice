384fc2b133c091d72ecfb37016f5ee272e081580
hoanganhminh <hoanganhminh120601@gmail.com>
2024-11-30 00:37:31 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Find All Duplicates in an Array/Solution.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Medium.Find_All_Duplicates_in_an_Array;
+
+public class Solution
+{
+    public IList<int> FindDuplicates(int[] nums)
+    {
+        HashSet<int> hsNums = [];
+        List<int> list = [];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (!hsNums.Add(nums[i]))
+            {
+                list.Add(nums[i]);
+            }
+        }
+
+        return list;
+    }
+}
