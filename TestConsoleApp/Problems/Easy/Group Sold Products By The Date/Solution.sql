59eee78a9cc3bf392e69c6b1ad426976d88c9c3b
hoanganhminh <hoanganhminh120601@gmail.com>
2024-12-03 23:52:12 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Group Sold Products By The Date/Solution.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+select
+a.sell_date,
+count(*) as num_sold,
+string_agg(a.product, ',' order by a.product) as products
+from 
+(
+    select distinct
+    sell_date,
+    product
+    from activities
+    order by sell_date
+) as a
+group by a.sell_date
+
+
