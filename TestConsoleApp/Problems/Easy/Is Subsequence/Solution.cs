4e3b626febe199b61acc921be98c657192f84ab5
hoanganhminh <hoanganhminh120601@gmail.com>
2024-12-08 15:49:06 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Is Subsequence/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy.Is_Subsequence;
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        if (t.Length < s.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            while (s[i] != t[i])
+            {
+                t = t.Remove(i, 1);
+
+                if (t.Length < s.Length)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
