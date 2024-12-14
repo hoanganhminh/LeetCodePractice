a5d1d884f6197fbf291a678ef6ec1d3a64a643ed
hoanganhminh <hoanganhminh120601@gmail.com>
2024-12-15 00:18:57 +0700

    Updated solution

--- TestConsoleApp/Problems/Easy/Single Number/Solution.cs
+++ TestConsoleApp/Problems/Easy/Single Number/Solution.cs
@@ -4,48 +4,24 @@ public class Solution
 {
     public int SingleNumber(int[] nums)
     {
-        bool flag = true;
+        Dictionary<int, bool> map = [];
 
-        for (int i = 0; i < nums.Length; i++)
+        foreach (int num in nums)
         {
-            flag = true;
-
-            for (int j = 0; j < nums.Length; j++)
+            if (!map.TryAdd(num, false))
             {
-                if (i == j)
-                {
-                    continue;
-                }
-
-                if (nums[j] == nums[i])
-                {
-                    flag = false;
-                    break;
-                }
+                map[num] = true;
             }
+        }
 
-            if (flag)
+        foreach (var item in map)
+        {
+            if (!item.Value)
             {
-                return nums[i];
+                return item.Key;
             }
         }
 
         return 0;
     }
-
-    //public int SingleNumber(int[] nums)
-    //{
-    //    if (nums.Length == 1)
-    //        return nums[0];
-
-    //    foreach (int num in nums)
-    //    {
-    //        if (nums.Where(x => x == num).Count() == 1)
-    //        {
-    //            return num;
-    //        }
-    //    }
-
-    //    return 0;
-    //}
 }
