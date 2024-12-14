63bf50179bee11e7244be506a8db12d7afedcfd8
hoanganhminh <hoanganhminh120601@gmail.com>
2024-12-15 00:19:15 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Medium/Single Number II/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Medium.Single_Number_II;
+
+public class Solution
+{
+    public int SingleNumber(int[] nums)
+    {
+        Dictionary<int, bool> map = [];
+
+        foreach (int num in nums)
+        {
+            if (!map.TryAdd(num, false))
+            {
+                map[num] = true;
+            }
+        }
+
+        foreach (var item in map)
+        {
+            if (!item.Value)
+            {
+                return item.Key;
+            }
+        }
+
+        return 0;
+    }
+}
