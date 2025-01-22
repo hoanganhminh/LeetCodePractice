fec97bd24f9b014e26f41f66720370b1697552ef
hoanganhminh <hoanganhminh120601@gmail.com>
2025-01-22 23:29:31 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Counter/Solution.js
@@ -0,0 +1,21 @@
+﻿/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+    var count = 0;
+
+    return function () {
+        var result = n + count;
+        count++;
+
+        return result;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
