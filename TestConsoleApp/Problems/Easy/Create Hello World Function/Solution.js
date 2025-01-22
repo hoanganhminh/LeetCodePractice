7fe036dbbf468b9b2c524bc6ae6f35c2ef9a85b7
hoanganhminh <hoanganhminh120601@gmail.com>
2025-01-22 23:28:38 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Create Hello World Function/Solution.js
@@ -0,0 +1,14 @@
+﻿/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+
+    return function (...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
