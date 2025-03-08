7beb957aeda42eff011463626a10d6d5c0b7cc15
hoanganhminh <hoanganhminh120601@gmail.com>
2025-03-08 23:48:44 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Find Winner on a Tic Tac Toe Game/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy.Find_Winner_on_a_Tic_Tac_Toe_Game;
+
+public class Solution
+{
+    public string Tictactoe(int[][] moves)
+    {
+        if (moves.Length < 5) return "Pending";
+
+        var aMoves = new List<int[]>();
+        var bMoves = new List<int[]>();
+
+        for (int i = 0; i < moves.Length; i++)
+        {
+            if (i % 2 == 0)
+            {
+                aMoves.Add(moves.ElementAt(i));
+            }
+            else
+            {
+                bMoves.Add(moves.ElementAt(i));
+            }
+        }
+
+        for (int i = 0; i < 2; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (aMoves.Select(x => x[i]).Count(x => x == j) == 3)
+                {
+                    return "A";
+                }
+
+                if (bMoves.Select(x => x[i]).Count(x => x == j) == 3)
+                {
+                    return "B";
+                }
+            }
+        }
+
+        if (aMoves.Count(x => x[0] == x[1]) == 3)
+        {
+            return "A";
+        }
+
+        if (bMoves.Count(x => x[0] == x[1]) == 3)
+        {
+            return "B";
+        }
+
+        if (aMoves.Count(x => x[0] + x[1] == 2) == 3)
+        {
+            return "A";
+        }
+
+        if (bMoves.Count(x => x[0] + x[1] == 2) == 3)
+        {
+            return "B";
+        }
+
+        return moves.Length == 9 ? "Draw" : "Pending";
+    }
+}
