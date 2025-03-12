256fc4e299786f98fffedd2bd3c7bd9c0850c1ce
hoanganhminh <hoanganhminh120601@gmail.com>
2025-03-13 01:14:15 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Repeated Substring Pattern/Solution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy.Repeated_Substring_Pattern;
+
+public class Solution
+{
+    public bool RepeatedSubstringPattern(string s)
+    {
+        List<int> divisors = [];
+        var sLength = s.Length;
+
+        if (sLength == 1)
+        {
+            return false;
+        }
+
+        for (int i = 1; i <= Math.Sqrt(sLength); i++)
+        {
+            if (sLength % i == 0)
+            {
+                divisors.Add(i);
+
+                if (i != sLength / i && sLength != sLength / i)
+                {
+                    divisors.Add(sLength / i);
+                }
+            }
+        }
+
+        foreach (int i in divisors)
+        {
+            var n = sLength / i;
+            var hsString = new HashSet<string>();
+
+            for (int j = 0; j < n; j++)
+            {
+                var part = s.Substring(j * (sLength / n), sLength / n);
+                hsString.Add(part);
+
+                if (hsString.Count > 1)
+                {
+                    break;
+                }
+            }
+
+            if (hsString.Count == 1)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
