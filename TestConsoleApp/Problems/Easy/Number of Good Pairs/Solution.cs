161f50eee1b4e8ad50c738bfdd8bd9af55f4a40a
hoanganhminh <hoanganhminh120601@gmail.com>
2025-04-29 00:06:23 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/Number of Good Pairs/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy.Number_of_Good_Pairs;
+
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var count = 0;
+
+        for (var i = 0; i < nums.Length; i++)
+        {
+            for (var j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j])
+                {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
