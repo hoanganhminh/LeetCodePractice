a7f0a2d36e097135899fc127f31861252225c562
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:53:05 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/1629. Slowest Key/Solution.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._1629._Slowest_Key;
+
+public class Solution
+{
+    public char SlowestKey(int[] releaseTimes, string keysPressed)
+    {
+        var currentLongestDuration = releaseTimes[0];
+        var currentLatgestDurationIndex = 0;
+
+        for (var i = 1; i < releaseTimes.Length; i++)
+        {
+            var currentDuration = releaseTimes[i] - releaseTimes[i - 1];
+            if (currentLongestDuration < currentDuration)
+            {
+                currentLatgestDurationIndex = i;
+                currentLongestDuration = currentDuration;
+            }
+            else if (currentLongestDuration == currentDuration)
+            {
+                currentLatgestDurationIndex = keysPressed[i] - keysPressed[currentLatgestDurationIndex] > 0 ? i : currentLatgestDurationIndex;
+            }
+        }
+
+        return keysPressed[currentLatgestDurationIndex];
+    }
+}
