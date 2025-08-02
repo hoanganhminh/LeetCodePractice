3cda15a69ba59df32f28581d709fb8393442ee1d
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:30:13 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/205. Isomorphic Strings/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._205._Isomorphic_Strings;
+
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        var dic = new Dictionary<char, char>();
+        var usedValue = new Dictionary<char, bool>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!dic.ContainsKey(s[i]))
+            {
+                if (!usedValue.TryAdd(t[i], true))
+                {
+                    return false;
+                }
+                else
+                {
+                    dic.Add(s[i], t[i]);
+                }
+            }
+            else
+            {
+                if (dic[s[i]] != t[i]) { return false; }
+            }
+        }
+
+        return true;
+    }
+}
