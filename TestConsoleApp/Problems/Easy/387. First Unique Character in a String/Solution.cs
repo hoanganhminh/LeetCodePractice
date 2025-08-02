f9814fd59f37c5c9299512d79f42853aa01b6b14
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:17:54 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/387. First Unique Character in a String/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._387._First_Unique_Character_in_a_String;
+
+public class Solution
+{
+    public int FirstUniqChar(string s)
+    {
+        var d = new Dictionary<char, int>();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!d.TryAdd(s[i], i))
+            {
+                d[s[i]] = -1;
+            }
+        }
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (d.TryGetValue(s[i], out var c))
+            {
+                if (c >= 0)
+                {
+                    return c;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
