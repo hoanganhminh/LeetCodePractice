f40822a620ccdc5fbb3f25b7e36915eb910dcb41
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:01:53 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/448. Find All Numbers Disappeared in an Array/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._448._Find_All_Numbers_Disappeared_in_an_Array;
+
+public class Solution
+{
+    public IList<int> FindDisappearedNumbers(int[] nums)
+    {
+        var d = new Dictionary<int, bool>();
+        var rs = new List<int>();
+
+        foreach (var x in nums)
+        {
+            d.TryAdd(x, true);
+        }
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!d.ContainsKey(i))
+            {
+                rs.Add(i);
+            }
+        }
+
+        return rs;
+    }
+}
