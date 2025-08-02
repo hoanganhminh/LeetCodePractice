d1fddc3602dd3836373081b99fff8098d34bf2e2
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:52:01 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/414. Third Maximum Number/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._414._Third_Maximum_Number;
+
+public class Solution
+{
+    public int ThirdMax(int[] nums)
+    {
+        var dic = new Dictionary<int, bool>();
+        var min = int.MinValue;
+        var max = min;
+        var max2nd = min;
+        var max3th = min;
+
+        foreach (var item in nums)
+        {
+            dic.TryAdd(item, false);
+        }
+
+        foreach (var item in dic.Keys)
+        {
+            if (item > max) { max3th = max2nd; max2nd = max; max = item; }
+            else if (item > max2nd) { max3th = max2nd; max2nd = item; }
+            else if (item > max3th) { max3th = item; }
+        }
+
+        if (dic.Keys.Count >= 3) { return max3th; }
+
+        return max;
+    }
+}
