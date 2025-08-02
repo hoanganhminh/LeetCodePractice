79d7f19577f40997f57f6cc3a574f8ff08e79524
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 14:59:25 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/168. Excel Sheet Column Title/Solution.cs
@@ -0,0 +1,18 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._168._Excel_Sheet_Column_Title;
+
+public class Solution
+{
+    public string ConvertToTitle(int columnNumber)
+    {
+        var result = "";
+
+        while (columnNumber > 0)
+        {
+            var second = (columnNumber - 1) % 26;
+            result = (char)(second + 65) + result;
+            columnNumber = (columnNumber - 1) / 26;
+        }
+
+        return result;
+    }
+}
