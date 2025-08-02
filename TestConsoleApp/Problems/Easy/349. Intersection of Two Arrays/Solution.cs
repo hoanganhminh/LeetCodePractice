1909a4b19ad19c2d5dbd0425dcfed4cb28ea35ca
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:49:58 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/349. Intersection of Two Arrays/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._349._Intersection_of_Two_Arrays;
+
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
+        var dic = new Dictionary<int, bool>();
+        var rs = new Dictionary<int, bool>();
+
+        foreach (int i in nums1)
+        {
+            dic.TryAdd(i, true);
+        }
+
+        foreach (int i in nums2)
+        {
+            if (dic.ContainsKey(i))
+            {
+                rs.TryAdd(i, true);
+            }
+        }
+
+        return [.. rs.Keys];
+    }
+}
