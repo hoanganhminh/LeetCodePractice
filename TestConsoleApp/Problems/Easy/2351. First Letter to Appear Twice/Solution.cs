3c0fc05dd76542ec3555b4d346f0ab5670d1dce2
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:23:22 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/2351. First Letter to Appear Twice/Solution.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._2351._First_Letter_to_Appear_Twice;
+
+public class Solution
+{
+    public char RepeatedCharacter(string s)
+    {
+        var d = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (!d.TryAdd(s[i], 1))
+            {
+                return s[i];
+            }
+        }
+
+        return 'a';
+    }
+}
