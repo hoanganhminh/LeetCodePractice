d21e2a651192e62c2674978bc2639efd76bbcb81
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:43:22 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/1446. Consecutive Characters/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._1446._Consecutive_Characters;
+
+public class Solution
+{
+    public int MaxPower(string s)
+    {
+        var streak = 1;
+        var currentStreak = 1;
+
+        for (int i = 1; i < s.Length; i++)
+        {
+            if (s[i] == s[i - 1])
+            {
+                currentStreak++;
+                streak = currentStreak > streak ? currentStreak : streak;
+            }
+            else
+            {
+                currentStreak = 1;
+            }
+        }
+
+        return streak;
+    }
+}
