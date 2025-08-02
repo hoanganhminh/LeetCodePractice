0969e23d2893025a6015df52ea0e90a9546f3d09
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 15:06:41 +0700

    Updated solution

--- TestConsoleApp/Problems/Medium/442. Find All Duplicates in an Array/Solution.cs
+++ TestConsoleApp/Problems/Medium/442. Find All Duplicates in an Array/Solution.cs
@@ -1,15 +1,15 @@
-﻿namespace LeetCodeSolutionsProject.Problems.Medium.Find_All_Duplicates_in_an_Array;
+﻿namespace LeetCodeSolutionsProject.Problems.Medium._442._Find_All_Duplicates_in_an_Array;
 
 public class Solution
 {
     public IList<int> FindDuplicates(int[] nums)
     {
-        HashSet<int> hsNums = [];
+        var d = new Dictionary<int, bool>();
         List<int> list = [];
 
         for (int i = 0; i < nums.Length; i++)
         {
-            if (!hsNums.Add(nums[i]))
+            if (!d.TryAdd(nums[i], false))
             {
                 list.Add(nums[i]);
             }
