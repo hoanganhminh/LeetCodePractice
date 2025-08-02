d10fe88e3b1c491403b6b7a489f24c9d848d24e0
hoanganhminh <hoanganhminh120601@gmail.com>
2025-08-02 14:49:28 +0700

    Added solution

---
+++ TestConsoleApp/Problems/Easy/645. Set Mismatch/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutionsProject.Problems.Easy._645._Set_Mismatch;
+
+public class Solution
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        var d = new Dictionary<int, bool>();
+        var rs = 0;
+
+        foreach (var i in nums)
+        {
+            if (!d.TryAdd(i, true))
+            {
+                rs = i;
+            }
+        }
+
+        for (int i = 1; i <= nums.Length; i++)
+        {
+            if (!d.ContainsKey(i))
+            {
+                return [rs, i];
+            }
+        }
+
+        return [];
+    }
+}
